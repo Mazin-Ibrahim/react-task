7680cb73b992878921044317d71866987ef98733
mezo <mazinabrahim200@gmail.com>
2020-12-13 13:46:24 -0700

    first commit

--- src/components/todo.jsx
+++ src/components/todo.jsx
@@ -20,40 +20,43 @@ class Todo extends Component {
          this.setState({lists:this.state.lists})
     }
 
+    restTasks = () => {
+        this.state.lists = []
+        this.setState({lists:this.state.lists})
+    }
+
     
     render() {
-        return <div className="container mx-auto">
-            <div className="mt-20">
-                <div className="mx-auto">
-                    <div className="mx-32">
-                        <div>
-                            <div>
-                                <div className="font-bold text-2xl my-4 text-white">
-                                    Task Title
-                               </div>
-                                <input onChange={ this.getTask } className="px-2 py-2 w-full" />
-                                <button className="bg-white my-4 py-2 px-1 font-bold rounded" onClick={this.newTask}>ADD NEW</button>
-                            </div>
+        return (
+          <div className="mt-10 mx-auto container">
+              <div className="md:w-full">
+                   <div className="p-4 rounded">
+                        <div className="text-center">
+                     <span className="font-semibold text-gray-700 text-2xl">Todo</span>
                         </div>
-                    </div>
-                </div>
-            </div>
+                        <div className="mt-4 p-4 shadow-md rounded md:w-3/4 w-full mx-auto">
+                                <div className="flex justify-between items-center my-8">
+                                <input onChange={this.getTask} className="bg-white px-2 py-1 border-gray-700 focus:border-indigo-400 focus:shadow-none focus:bg-white mt-1 block w-full" type="text"/>
+                                <button onClick={this.newTask} className="px-2 py-1 h-10 w-14 bg-green-500 hover:bg-green-600 text-white text-sm font-medium rounded mx-2">Add</button>
+                               </div>
 
-            <div className="my-4 container mx-auto">
-             <div className="mx-32">
-                  {this.state.lists.map((list,index)=>(
-                      <ul>
-                          <div key={index} className="w-full bg-white p-4 rounded-md shadow-md my-1 flex justify-between items-center">
-                              <li>
-                                  {list}
-                              </li>
-                              <button onClick={() => this.deleteItem(index)}  className="font-bold text-2xl">x</button>
-                          </div>
-                      </ul>
-                  ))}  
-             </div>
-            </div>
-        </div>;
+                            {this.state.lists.map((list, index) => (
+                                <div key={index} className="bg-white rounded hover:shadow-lg p-2 flex justify-between items-center mt-2">
+                                    <span className="font-semibold text-gray-500">{list}</span>
+                                    <button onClick={() => this.deleteItem(index)} className="font-semibold text-2xl">x</button>
+                                </div>
+                            ))} 
+                        </div>
+
+                        <div className="text-center mt-4">
+                            <button onClick={this.restTasks} className="px-2 py-1 bg-red-300 shadow-md rounded text-white">Rest</button>
+                        </div>
+                        
+                   </div>
+              </div>
+          </div>
+        );
+        
     }
 }
 
